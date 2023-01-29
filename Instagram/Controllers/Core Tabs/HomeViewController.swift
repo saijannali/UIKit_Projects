485f3a672a9fac196641ec47eed4fd608e8673b7
Sai Jannali <sjannali@iu.edu>
2023-01-28 18:26:00 -0600

    first commit

---
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Sai Jannali on 1/28/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
